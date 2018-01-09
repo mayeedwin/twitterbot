5366a10e2b769608205a84a959412c4ef91df83f
Maye Edwin <mayedwine1@gmail.com>
2018-01-09 10:51:46 +0300

    Update and rename tweetmayebot.js to tweetbot.js

--- tweetbot.js
+++ tweetbot.js
@@ -4,8 +4,8 @@ var Twit = require('twit');
 // We need to include our configuration file
 var T = new Twit(require('./config.js'));
 
-// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
+// This is the URL of a search for the latest tweets on the '#MeetMaye' hashtag.
+var mediaArtsSearch = {q: "#MeetMaye", count: 10, result_type: "recent"}; 
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
@@ -36,6 +36,6 @@ function retweetLatest() {
 
 // Try to retweet something as soon as we run the program...
 retweetLatest();
-// ...and then every hour after that. Time here is in milliseconds, so
+// ...and then every hour/half after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
+setInterval(retweetLatest, 1000 * 60 * 30);
