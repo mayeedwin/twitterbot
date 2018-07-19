115b24ad958cfbc763f6652c40e00f832cd7f9d3
Maye Edwin <Edwin@impala.com>
2018-07-19 08:31:33 +0300

    add bot dir

--- bot/retweetbot.js
+++ bot/retweetbot.js
@@ -5,7 +5,7 @@ var Twit = require('twit');
 var T = new Twit(require('./config.js'));
 
 // This is the URL of a search for the latest tweets on the '#MeetMaye' hashtag.
-var mediaArtsSearch = {q: "#MeetMaye", count: 10, result_type: "recent"}; 
+var mediaArtsSearch = {q: "#MeetMaye", count: 100, result_type: "recent"}; 
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
