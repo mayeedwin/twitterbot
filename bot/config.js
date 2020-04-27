1d06d8395d5bf91e3f91aef27c7974401a747060
Maye Edwin <mayedwine1@gmail.com>
2020-04-28 02:19:09 +0300

    Ignore npm

--- bot/config.js
+++
@@ -1,7 +0,0 @@
-// Put your own Twitter App keys here. See README.md for more detail.
-module.exports = {
-  consumer_key:         'API key',
-  consumer_secret:      'API secret key',
-  access_token:         'Access token',
-  access_token_secret:  'Access token secret'
-}
