5f2515d846cdbb09a77f5b93bbea4f3a362ca093
Maye Edwin <mayedwine1@gmail.com>
2020-04-28 02:21:04 +0300

    Updated

---
+++ bot/config.js
@@ -0,0 +1,8 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+    consumer_key:         'API key',
+    consumer_secret:      'API secret key',
+    access_token:         'Access token',
+    access_token_secret:  'Access token secret'
+  }
+  
