a7b15ed8f83b302f166bc95bcd8168dad798135d
Theefesto <128065230+Theefesto@users.noreply.github.com>
2023-05-20 14:01:52 +0300

    Update config_sample.js

--- config_sample.js
+++ config_sample.js
@@ -1,8 +1,8 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-    consumer_key:         'API key',
-    consumer_secret:      'API secret key',
-    access_token:         'Access token',
-    access_token_secret:  'Access token secret'
+    consumer_key:         'tNbjfTpZ2wo6ImpFHPj4i0EtytNbjfTpZ2wo6ImpFHPj4i0Ety',
+    consumer_secret:      'zOl1Q89MZuiy8fm8d3JR4IF8lCuIiDw1igYMPeUeiT8YzX5Ete',
+    access_token:         '1648786274776498179-53v1hlho78Br9DBvmZAA5CG20au8vI',
+    access_token_secret:  '1sc1W2Swf6uj1fe6F1jJ93zjPtGMOfIY0biHDvfAFFwOY'
   }
   
