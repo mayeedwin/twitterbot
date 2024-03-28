613960c6b12bd44d1ccaa8f4b5b99eaff937c268
kibrahim9101 <kibrahim9101@outlook.com>
2024-03-28 05:52:22 +0000

    Organized code.

--- bot/bot.js
+++ bot/bot.js
@@ -5,44 +5,46 @@ const Twit = require('twit');
 const twit = new Twit(require('./config.js'));
 
 // This is the URL of a search for the latest tweets from a group of Palestine News Accounts
-const searchQuery = { q: 'from%3Amiddleeasteye%20OR%20from%3Aeyeonpalestine%20OR%20from%3Awolpalestine%20OR%20from%3Aqudsnen%20OR%20from%3Apalyouthmvmt%20OR%20from%3Apalestinercs)%20-filter%3Areplies', count: 50, result_type: 'mixed', include_entities: false };
+const searchQuery = { q: 'from%3Amiddleeasteye%20OR%20from%3Aeyeonpalestine%20OR%20from%3Awolpalestine%20OR%20from%3Aqudsnen%20OR%20from%3Apalyouthmvmt%20OR%20from%3Apalestinercs)', count: 50, result_type: 'mixed', include_entities: false };
 //Changed to get the 50 most recent tweets instead of 100
 
 //will contain a string for a list I made for Palestine News Sources. I am changing the logic to
 //Pull tweets from a twitter list instead of tweets that match a twitter search.
-const palestineListId = { list_id:  '1746614491717022134', count: 50, include_rts: false, include_entities: false};
+const palestineListId = { id:  '1746614491717022134',  max_results: 50 };
 // This function finds the latest tweet that matches the search above and retweets.
 const retweetLatest = async() => {
-  try {
-	//The endpoint for tweets sent by members of a list is in lists/statusues
-	//twit.get('lists/statuses', palestineListId, (error, data, response) => {
-      twit.get('search/tweets', searchQuery, (error, data) => {
-    // If our search request to the server had no errors...
-    if (error) {
-      // However, if our original search request had an error, we want to print it out here...
-      console.log(error.message);
-//	console.log(respsonse.message);
-    } else {
-	//creating a loop to retweet the entire list of tweets
-	for (let i = 0; i < data.statuses.length; i++){
-      // Grab the ID of the tweet we want to retweetwit...
-      const retweetId = data.statuses[i].id_str;
-      // Tell Twitter we want to retweet it...
-      twit.post('statuses/retweet/' + retweetId, {}, (error, response) => {
-        // If there was an error with our Twitter call, we print it out here...
-        if (error) {
-          console.log(error.message);
-        } else if (response) {
-          console.log('Success! Retweeted!');
-        }
-      });
-	}
-    }
-  });
-  } catch(error) {
+	try {
+
+		//The endpoint for tweets sent by members of a list is in lists/:id/tweets
+		//twit.get('lists/:id/tweets', palestineListId, (error, data, response) => {
+		twit.get('search/tweets', searchQuery, (error, data) => {
+			// If our search request to the server had no errors...
+			if (error) {
+     			 	// However, if our original search request had an error, we want to print it out here...
+      				console.log(error.message);
+	    		//} else if (response){
+				//console.log(response.message);}
+			 else {
+				//creating a loop to retweet the entire list of tweets
+				for (let i = 0; i < data.statuses.length; i++){
+					 // Grab the ID of the tweet we want to retweetwit...
+		      			const retweetId = data.statuses[i].id_str;
+     					 // Tell Twitter we want to retweet it...
+      					twit.post('statuses/retweet/' + retweetId, {}, (error, response) => {
+		        		// If there was an error with our Twitter call, we print it out here...
+	        			if (error) {
+        		  			console.log(error.message);
+	        			} else if (response) {
+		        	  		console.log('Success! Retweeted!');
+	        			}
+      					});
+				}
+			}
+  		});
+  	} catch(error) {
      // Handle errors...
     console.log(error)
-  }
+  	}
 };
 
 // Try to retweet something as soon as we run the program...
